a2e57977c24bda502b0301d0ba03aefd48dfb3ad
WilliamStar007 <anda.liang@vanderbilt.edu>
2022-01-17 16:19:47 -0600

    a different approach for background color

--- main.py
+++ main.py
@@ -14,9 +14,24 @@ with Image.open(input("image name: ")) as im:
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0, 0) + (size, size), fill=255)
 
-    arr = np.random.randint(low=50, high=90, size=(size, size))
-    new_im = Image.fromarray(arr.astype('uint8'), 'L')
-    new_im = new_im.convert('RGB')
+    # generate grey background
+    new_im = Image.new("RGB", (size//8, size//8))
+    np_img = np.array(new_im)
+
+    for r in range(0, size//8):
+        for c in range(0, size//8):
+            re = random.randint(60, 80) + random.randint(0, c//50) - random.randint(0, r//50) + random.randint(-3, 3)
+            gr = random.randint(60, 80) + random.randint(0, c//50) - random.randint(0, r//50) + random.randint(-3, 3)
+            bl = random.randint(60, 80) + random.randint(0, c//50) - random.randint(0, r//50) + random.randint(-3, 3)
+
+            np_img[r][c] = [re, gr, bl]
+
+    new_im = Image.fromarray(np_img, 'RGB')
+    new_im = new_im.resize((size, size))
+
+    # arr = np.random.randint(low=50, high=90, size=(size, size))
+    # new_im = Image.fromarray(arr.astype('uint8'), 'L')
+    # new_im = new_im.convert('RGB')
     # new_im = Image.new('RGB', (size, size), (105, 105, 105))
 
     new_im.paste(im, (int((size - x) / 2), int((size - y) / 2)))
