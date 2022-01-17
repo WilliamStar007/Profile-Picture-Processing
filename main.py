505af82a2617809ee6f1dc23dbe407758976c4cf
WilliamStar007 <anda.liang@vanderbilt.edu>
2022-01-17 02:17:31 -0600

    Crop to circular shape Background needs work

---
+++ main.py
@@ -0,0 +1,27 @@
+# main.py
+from PIL import Image, ImageOps, ImageDraw
+import numpy as np
+import random
+
+# open a target image
+with Image.open(input("image name: ")) as im:
+    # current image size
+    x, y = im.size
+    size = max(x, y)
+
+    # a transparent mask
+    mask = Image.new('L', (size, size), 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + (size, size), fill=255)
+
+    arr = np.random.randint(low=90, high=105, size=(size//3, size//3, 3))
+    new_im = Image.fromarray(arr.astype('uint8'))
+    new_im = new_im.resize((size, size))
+    # new_im = Image.new('RGB', (size, size), (105, 105, 105))
+
+    new_im.paste(im, (int((size - x) / 2), int((size - y) / 2)))
+
+    output = ImageOps.fit(new_im, mask.size, centering=(0.5, 0.5))
+    output.putalpha(mask)
+
+    output.save('output.png')
