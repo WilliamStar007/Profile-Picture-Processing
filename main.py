8631c53be6f31d453e7a7e1c16196ae16eb32766
WilliamStar007 <anda.liang@vanderbilt.edu>
2022-01-18 14:40:48 -0600

    fixed background for cropping!

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # main.py
 from PIL import Image, ImageOps, ImageDraw
 import numpy as np
-import random
 
 # open a target image
-with Image.open(input("image name: ")) as im:
+image_name = input("image name: ")
+with Image.open(image_name) as im:
     # current image size
     x, y = im.size
     size = max(x, y)
@@ -15,13 +15,18 @@ with Image.open(input("image name: ")) as im:
     draw.ellipse((0, 0) + (size, size), fill=255)
 
     # generate grey background
-    arr = np.random.randint(low=60, high=70, size=(size, size, 3))
-    new_im = Image.fromarray(arr.astype('uint8'))
-    # new_im = Image.new('RGB', (size, size), (100, 100, 100))
+    np_data = np.array(im)
+    back_grd_left = Image.fromarray(np_data[0:3543, 0:910])
+    back_grd_right = Image.fromarray(np_data[0:3543, 2430:2835])
+    grey_back_grd = Image.new('RGB', (size, size), (255, 255, 255))
 
-    new_im.paste(im, (int((size - x) / 2), int((size - y) / 2)))
+    grey_back_grd.paste(back_grd_left, (0, 0))
+    grey_back_grd.paste(back_grd_right, (2835, 0))
+    grey_back_grd.paste(back_grd_right, (3240, 0))
 
-    output = ImageOps.fit(new_im, mask.size, centering=(0.5, 0.5))
+    grey_back_grd.paste(im, (int((size - x) / 2), int((size - y) / 2)))
+
+    output = ImageOps.fit(grey_back_grd, mask.size, centering=(0.5, 0.5))
     output.putalpha(mask)
 
     output.save('output.png')
